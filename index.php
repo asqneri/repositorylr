13056be157ae4c6cc2c0f557f0b921f3eae288cc
asqneri <wirvorfi@gmail.com>
2025-11-07 16:51:43 +0300

    Добавили меню

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+function printMenu() {
+    echo "1. Ввести два числа\n";
+    echo "2. Сложение\n";
+    echo "3. Вычитание\n";
+    echo "4. Деление\n";
+    echo "5. Возведение в степень\n";
+}
+
+$a = 0;
+$b = 0;
+
+
+
+
+while (true) {
+    printMenu();
+    $choice = readline("Выберите пункт меню: ");
+
+    switch ($choice) {
+        case 1:
+            input_numbers($a, $b);
+            break;
+        case 2:
+            add($a, $b);
+            break;
+        case 3:
+            subtract($a, $b);
+            break;
+        case 4:
+            divide($a, $b);
+            break;
+        case 5:
+            power($a, $b);
+            break;
+        default:
+            echo "Неверный пункт\n";
+    }
+}
+?>
